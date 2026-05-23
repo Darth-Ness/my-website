6641bd35e02ca8b3922d423d629003660261870e
Darth-Ness <75047141+Darth-Ness@users.noreply.github.com>
2026-05-23 13:35:58 +0000

    Fix banner not loading for blog posts

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+if(window.location.href.indexOf("post") == -1) {  var url="banner.html"  }
+else {  var url = "../banner.html"  }
 fetch("banner.html")
     .then((response) => response.text())
     .then((data) => document.getElementById("banner").innerHTML = data);
