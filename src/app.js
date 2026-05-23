33edca14ac9991952d2d93f3e3834ddbf462213b
Darth-Ness <75047141+Darth-Ness@users.noreply.github.com>
2026-05-23 13:38:43 +0000

    This time fix it for real by ACTUALLY using the vairable

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 if(window.location.href.indexOf("post") == -1) {  var url="banner.html"  }
 else {  var url = "../banner.html"  }
-fetch("banner.html")
+fetch(url)
     .then((response) => response.text())
     .then((data) => document.getElementById("banner").innerHTML = data);
 
